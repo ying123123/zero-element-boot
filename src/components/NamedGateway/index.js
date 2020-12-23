63f2b129484817bc2921676df845b1ab904fabfa
huawei laptop <huawei@laptop.com>
2020-12-23 18:13:17 +0800

    fix NamedGateway

--- src/components/NamedGateway/index.js
+++ src/components/NamedGateway/index.js
@@ -1,10 +1,10 @@
 const React = require('react');
 const GatewaySet = require('../gateway');
 
-export default function NamedGateway({ name, props, children }) {
+export default function NamedGateway({ name, props, children, ...rest }) {
   const NamedGateway = GatewaySet[name] || tips(name);
 
-  return <NamedGateway {...props}>
+  return <NamedGateway {...props} {...rest} >
     {children}
   </NamedGateway>
 }
