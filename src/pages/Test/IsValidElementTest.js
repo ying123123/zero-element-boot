5551b21a74e04b27721b70a80cddbeeb03682a73
huawei laptop <huawei@laptop.com>
2020-12-24 10:44:01 +0800

    add test cases

---
+++ src/pages/Test/IsValidElementTest.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Foo = () => <div>foo</div>;
+
+class Bar extends React.Component {
+    render() {
+       return (
+          <div>bar</div>
+       )
+    }
+}
+
+const Gone = () => 0
+
+export default function IsValidElementTest(props) {
+
+    const content = 'string'
+
+    console.log('<Foo/>:', React.isValidElement(<Foo />));
+    console.log('Bar:', React.isValidElement(Bar));
+    console.log('<Ba/>:', React.isValidElement(<Bar />));
+    console.log('string:', React.isValidElement(content));
+    console.log('Gone:', React.isValidElement(Gone));
+    console.log('<Gone/>:', React.isValidElement(<Gone/>));
+
+    return <Bar/>
+}
+
+
+
