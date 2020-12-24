9ca0d58739ba0e2383389e53d9485491a5a92a57
huawei laptop <huawei@laptop.com>
2020-12-24 10:45:36 +0800

    fix AutoComponent NamedGateway

--- src/components/AutoComponent/index.js
+++ src/components/AutoComponent/index.js
@@ -1,12 +1,9 @@
 const React = require('react');
 const { useState, useEffect } = require('react');
-const {NamedLayout, NamedGateway, NamedCart} = require('@/components');
+const {NamedLayout, NamedGateway, NamedCart, NamedSeperator} = require('@/components');
 const {Gateway} = require('@/components/gateway');
-
-
 const useLayout = require('@/hooks/useLayout');
 const requireConfig = require('@/components/AutoX/requireConfig');
-const promiseAjax = require('@/utils/request');
 
 // const presenter = require('@/components/presenter');
 // const allComponents = {
@@ -15,57 +12,33 @@ const promiseAjax = require('@/utils/request');
 
 module.exports = function (props) {
   const parent = module.parents[0]; //get module name
-
   const { config = requireConfig(parent), allComponents={} } = props;
-
-
-  //获取 /public 配置文件所需代码
-  //start
-  const { cfgLayout={} } = config;
-  const { path='' } = cfgLayout;
-  const [restartRender, setRestartRender] = useState(0);
-  //end
-
-  const cfgData = path?undefined:config;
-
-  const [cfg, setCfg] = useState(cfgData);
-  const { layout, ...restCfg } = cfg || {};
-  const { children, ...restLayout } = layout || {};
   const [layoutRef, { getClassName }] = useLayout();
 
-  useEffect(_ => {
-    if (cfg === undefined) {
-      promiseAjax(`/x/${path}/layout.json`, {
-        _t: new Date().getTime(),
-      })
-        .then(data => {
-          setCfg(data);
-          setRestartRender(restartRender + 1)
-        })
-    }
-  }, []);
+  // get layout only 
+  const { layout, ...rest } = config || {}; 
+  const { children, ...restLayout } = layout || {};
 
+  // restLayout means layout props
+  // child iterator from children contains: [name, span, width, gateway, [,seperator, cart]]
   return <div
     className={getClassName()}
   >
-    {cfg ? (
       <NamedLayout {...restLayout} ref={layoutRef}>
         {children.map((child, i) => {
-          const { name, span, gateway } = child;
+          const { name, span, width, gateway } = child;
           const C = allComponents[name] || tips(name);
-          
-          let gatewayProps = { ...restCfg };
-          if (gateway && typeof gateway === 'object' && gateway.props) {
-            gatewayProps = { ...gatewayProps, ...gateway.props }
-          }
+
+          //get gateway name
+          const gatewayName = gateway ? (typeof gateway === 'string' ? gateway : gateway.name) : 'Gateway' 
+          const gatewayProps = gateway.props || {} 
 
           // each item has its Named Gateway
-          return <Gateway key={i} {...gatewayProps} span={span}>
+          return <NamedGateway key={i} name={gatewayName} {...gatewayProps} {...rest} span={span}>
                <C name={name} />
-          </Gateway>
+          </NamedGateway>
         })}
       </NamedLayout>
-    ) : ''}
   </div>;
 }
 
