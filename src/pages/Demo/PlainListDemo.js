0e246b7f00a07c3b239b0708fde3562c54045482
HDJ <263463764@qq.com>
2020-12-25 17:04:48 +0800

    整理 src/pages/Demo 代码

--- src/pages/Demo/PlainListDemo.js
+++ src/pages/Demo/PlainListDemo.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import PlainList from '@/components/list/PlainList';
-import NamedLayout from '@/components/NamedLayout';
-import NamedCart from '@/components/NamedCart';
+import { NamedList, NamedLayout, NamedCart } from "@/components";
 
 const { CartItem } = require('@/presenter/composition')
 
-export default function PlainListDemo(props){
+export default function PlainListDemo(props) {
 
     /**
      * cart 
@@ -15,34 +13,38 @@ export default function PlainListDemo(props){
 
 
     const config = {
-        items:[
-            {icon:'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png',  title:'title111',  subTitle:'subTitle111',},
-            {icon:'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png',  title:'title222',  subTitle:'subTitle222',},
-            {icon:'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png',  title:'title333',  subTitle:'subTitle333',},
-            {icon:'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png',  title:'title444',  subTitle:'subTitle444',},
-          ],
-        layout: {
-            name:'Flexbox',
-            props: {
-                align: 'start', 
-                direction: 'row',
+        items: [
+            { icon: 'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png', title: 'title111', subTitle: 'subTitle111', },
+            { icon: 'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png', title: 'title222', subTitle: 'subTitle222', },
+            { icon: 'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png', title: 'title333', subTitle: 'subTitle333', },
+            { icon: 'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png', title: 'title444', subTitle: 'subTitle444', },
+        ],
+        template: {
+            layout: {
+                name: 'Flexbox',
+                props: {
+                    align: 'start',
+                    direction: 'row',
+                },
             },
-        },
-        cart:{
-            name: 'HoverShadowCart',
-            props: {
-            },
+            cart: {
+                name: 'HoverShadowCart',
+                props: {
+                },
+            }
         }
+
 
     };
 
     return (
-        <PlainList {...config} >
+        <NamedList name='PlainList' {...config} >
             <NamedLayout>
                 <NamedCart>
                     <CartItem />
                 </NamedCart>
             </NamedLayout>
-        </PlainList>
+        </NamedList>
+
     )
 }
--- src/pages/Demo/RectangleListDemo.js
+++ src/pages/Demo/RectangleListDemo.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import PlainList from '@/components/list/PlainList';
-import NamedLayout from '@/components/NamedLayout';
-import NamedCart from '@/components/NamedCart';
+import { NamedList, NamedLayout, NamedCart } from "@/components";
 
 const { RectangleItem } = require('@/presenter/composition')
 
@@ -20,31 +18,34 @@ export default function RectangleListDemo(props){
             {icon:'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png',  title:'title333',  subTitle:'subTitle333',},
             {icon:'https://media.geeksforgeeks.org/wp-content/uploads/20200403151026/adblur_gfg.png',  title:'title444',  subTitle:'subTitle444',},
           ],
-        layout: {
-            name:'Flexbox',
-            props: {
-                align: 'start', 
-                direction: 'row',
-                itemStyle:{
-                    width: ''
-                }
+        template:{
+            layout: {
+                name:'Flexbox',
+                props: {
+                    align: 'start', 
+                    direction: 'row',
+                    itemStyle:{
+                        width: ''
+                    }
+                },
             },
-        },
-        cart:{
-            name: 'Rectangle',
-            props: {
-            },
+            cart:{
+                name: 'Rectangle',
+                props: {
+                },
+            }
         }
+        
 
     };
 
     return (
-        <PlainList {...config} >
+        <NamedList name='PlainList' {...config} >
             <NamedLayout>
                 <NamedCart>
                     <RectangleItem />
                 </NamedCart>
             </NamedLayout>
-        </PlainList>
+        </NamedList>
     )
 }
